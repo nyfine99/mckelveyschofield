c2999d07010b31f490ea86e4dc2a30e023081c4f
nyfine99 <noahyfine@gmail.com>
2024-07-23 01:14:31 -0400

    Plotting elections ability added

--- election_dynamics/election_dynamics.py
+++ election_dynamics/election_dynamics.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 from abc import ABC, abstractmethod
 
 from voters.voter import Voter
@@ -19,18 +21,74 @@ class ElectionDynamics(ABC):
         self.voters = voters
         self.evaluation_function = evaluation_function
 
+
+    @abstractmethod
+    def tabulate_votes(self):
+        pass
+
+
     @abstractmethod
     def compare_policies(self):
         pass
 
 
 class ElectionDynamicsTwoParty(ElectionDynamics):
-    def __init__(self, voters: list[Voter], evaluation_function: callable):
+    def __init__(self, voters: list[Voter], evaluation_function: callable, tiebreak_func: callable = None):
         self.voters = voters
         self.evaluation_function = evaluation_function
+        self.tiebreak_func = tiebreak_func # used when a voter is ambivalent to distribute their vote
+
+    def tabulate_votes(self, original_policy: Policy, new_policy: Policy):
+        counts = [0,0] # index 0 represents the original_policy count, index 1 the new policy
+        for voter in self.voters:
+            original_utility = voter.get_utility(original_policy)
+            new_utility = voter.get_utility(new_policy)
+            if original_utility > new_utility:
+                counts[0] += 1
+            elif original_utility == new_utility and self.tiebreak_func is not None:
+                # if == and no tiebreak_func, the voter doesn't vote
+                self.tiebreak_func()
+            elif original_utility < new_utility:
+                counts[1] += 1
+        return counts
 
     def compare_policies(self, original_policy: Policy, new_policy: Policy):
-        return self.evaluation_function(self.voters, original_policy, new_policy)   
+        return self.evaluation_function(self.tabulate_votes(original_policy, new_policy))
+    
+    def obtain_votes(self, original_policy: Policy, new_policy: Policy):
+        votes = len(self.voters)*[-1] # index 0 represents the original_policy count, index 1 the new policy
+        for i in range(0,len(self.voters)):
+            voter = self.voters[i]
+            original_utility = voter.get_utility(original_policy)
+            new_utility = voter.get_utility(new_policy)
+            if original_utility > new_utility:
+                votes[i] = 0
+            elif original_utility == new_utility and self.tiebreak_func is not None:
+                # if == and no tiebreak_func, the voter doesn't vote
+                self.tiebreak_func()
+            elif original_utility < new_utility:
+                votes[i] = 1
+        return votes
+    
+    def plot_election_2d(self, original_policy: Policy, new_policy: Policy):
+        votes = self.obtain_votes(original_policy, new_policy)
+        plt.figure(figsize=(8, 6))
+        # should probably make the winner have a star at some point or something
+        plt.scatter([original_policy.values[0]],[original_policy.values[1]], color='blue', marker='x')
+        plt.scatter([new_policy.values[0]],[new_policy.values[1]], color='red', marker='x')
+        for i in range(len(self.voters)):
+            voter = self.voters[i]
+            color = 'yellow'
+            if votes[i] == 0:
+                color = 'blue'
+            elif votes[i] == 1:
+                color = 'red'
+            plt.scatter([voter.ideal_policy.values[0]], [voter.ideal_policy.values[1]], color=color, marker='o')
+        plt.title('Voters\' Distribution')
+        # plt.xlabel(issues[0])
+        # plt.ylabel(issues[1])
+        plt.grid(True)
+        plt.show()
 
 
 class ElectionDyanamicsMultiParty(ElectionDynamics):
--- election_dynamics/electoral_systems.py
+++ election_dynamics/electoral_systems.py
@@ -2,18 +2,10 @@ from election_dynamics.election_dynamics import ElectionDynamicsTwoParty
 from voters.voter import Voter
 
 
-def status_quo_preference(voters: list[Voter], original_policy, new_policy):
+def status_quo_preference(counts: list[int]):
     """
     Returns 0 if the original policy is preferred, and 1 if the new policy is preferred.
     """
-    counts = [0,0] # index 0 represents the original_policy count, index 1 the new policy
-    for voter in voters:
-        original_utility = voter.get_utility(original_policy)
-        new_utility = voter.get_utility(new_policy)
-        if original_utility >= new_utility:
-            counts[0] += 1
-        else:
-            counts[1] += 1
     if counts[0] >= counts[1]:
         return 0
     return 1
